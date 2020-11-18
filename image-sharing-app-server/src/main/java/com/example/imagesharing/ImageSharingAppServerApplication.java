a7b71e8f57a494c7f2151d5b8200b82c63c6432d
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-11-18 19:54:53 +0200

    Initial commit

---
+++ image-sharing-app-server/src/main/java/com/example/imagesharing/ImageSharingAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSharingAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSharingAppServerApplication.class, args);
+	}
+
+}
---
+++ image-sharing-app-server/src/test/java/com/example/imagesharing/ImageSharingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSharingAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
