401497e0edf4d4b343f71cf5a6c893b47782d744
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-11-25 20:04:30 +0200

    Added Spring Data JPA auditing

---
+++ image-sharing-app-server/src/main/java/com/example/imagesharing/audit/BaseAudit.java
@@ -0,0 +1,31 @@
+package com.example.imagesharing.audit;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedBy
+    protected Long createdBy;
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedBy
+    protected Long modifiedBy;
+
+    @LastModifiedDate
+    protected LocalDateTime modifiedAt;
+}
---
+++ image-sharing-app-server/src/main/java/com/example/imagesharing/config/AuditingConfig.java
@@ -0,0 +1,40 @@
+package com.example.imagesharing.config;
+
+
+import com.example.imagesharing.security.UserPrincipal;
+import com.sun.istack.Nullable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditingConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+    static class AuditorAwareImpl implements AuditorAware<Long> {
+
+        @Override
+        public Optional<Long> getCurrentAuditor() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+
+            UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+            return Optional.of(userPrincipal.getId());
+        }
+    }
+
+}
