9652128da1afa757ed0003553d1684da0cd5b686
Vasilis Nikolaou <basilisnik16@gmail.com>
2021-04-07 15:23:37 +0300

    added .gitignore to root

---
+++ image-sharing-app-server/src/main/java/com/example/imagesharing/exceptionhandling/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.imagesharing;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
---
+++ image-sharing-app-server/src/main/java/com/example/imagesharing/payload/ApiError.java
@@ -0,0 +1,4 @@
+package com.example.imagesharing.payload;
+
+public class ApiError {
+}
